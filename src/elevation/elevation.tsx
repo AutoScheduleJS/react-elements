760afce2227f9c8d21a815ff0fcd50050f1faf24
PBM42 <pbm764927@gmail.com>
2018-12-22 14:38:33 +0100

    feat: add elevation props

---
+++ src/elevation/elevation.tsx
@@ -0,0 +1,153 @@
+import { css } from 'emotion';
+import { merge } from '../util/hoc.util';
+
+interface ElevationTheme {
+  shadows: {
+    baselineColor: string;
+  };
+}
+
+const defaultTheme = (theme: any): ElevationTheme =>
+  merge(
+    {
+      shadows: {
+        baselineColor: '#000000',
+      },
+    },
+    theme
+  );
+
+const umbraOpacity = '33';
+const penumbraOpacity = '24';
+const ambientOpacity = '1F';
+
+const umbraZValue: ReadonlyArray<string> = [
+  '0px 0px 0px 0px',
+  '0px 2px 1px -1px',
+  '0px 3px 1px -2px',
+  '0px 3px 3px -2px',
+  '0px 2px 4px -1px',
+  '0px 3px 5px -1px',
+  '0px 5px 5px -3px',
+  '0px 7px 8px -4px',
+  '0px 8px 10px -5px',
+  '0px 11px 15px -7px',
+];
+
+const penumbraZValue: ReadonlyArray<string> = [
+  '0px 0px 0px 0px',
+  '0px 1px 1px 0px',
+  '0px 2px 2px 0px',
+  '0px 3px 4px 0px',
+  '0px 4px 5px 0px',
+  '0px 6px 10px 0px',
+  '0px 8px 10px 1px',
+  '0px 12px 17px 2px',
+  '0px 16px 24px 2px',
+  '0px 24px 38px 3px',
+];
+
+const ambiantZValue: ReadonlyArray<string> = [
+  '0px 0px 0px 0px',
+  '0px 1px 3px 0px',
+  '0px 1px 5px 0px',
+  '0px 1px 8px 0px',
+  '0px 1px 10px 0px',
+  '0px 1px 18px 0px',
+  '0px 3px 14px 2px',
+  '0px 5px 22px 4px',
+  '0px 6px 30px 5px',
+  '0px 9px 46px 8px',
+];
+
+const ElevationRootStyles = (theme: ElevationTheme, elevation: number) => {
+  const color = theme.shadows.baselineColor;
+  const eleIndex = [0, 1, 2, 3, 4, 6, 8, 12, 16, 24]
+    .map((val, i) => ({ elevation: val, distance: Math.abs(val - elevation), index: i }))
+    .reduce((acc, cur) => (acc.distance < cur.distance ? acc : cur)).index;
+  const umbraColor = color + umbraOpacity;
+  const penumbraColor = color + penumbraOpacity;
+  const ambiantColor = color + ambientOpacity;
+  return css`
+    transition: box-shadow 100ms cubic-bezier(0.4, 0, 0.2, 1);
+    box-shadow: ${umbraZValue[eleIndex]} ${umbraColor}, ${penumbraZValue[eleIndex]} ${penumbraColor},
+      ${ambiantZValue[eleIndex]} ${ambiantColor};
+  `;
+};
+
+export const ElevationProps = (elevation: number, customTheme?: any) => {
+  const theme = defaultTheme(customTheme);
+  return {
+    className: ElevationRootStyles(theme, elevation),
+  };
+};
+
+export const ElevationPropsToggle = (
+  stateHandler: {
+    state: { elevation: number };
+    setState: (v: any) => void;
+  },
+  inactive: number,
+  active: number,
+  eventNameIn: string,
+  eventNameOut: string,
+  customTheme: any
+) => {
+  const theme = defaultTheme(customTheme);
+  const { state, setState } = stateHandler;
+  let elevation = state.elevation;
+  const handleMouseDown = () => {
+    setState({
+      elevation: active,
+    });
+    addEventListener(eventNameOut, handleMouseUp);
+  };
+  const handleMouseUp = () => {
+    setState({
+      elevation: inactive,
+    });
+    removeEventListener(eventNameOut, handleMouseUp);
+  };
+  return {
+    className: ElevationRootStyles(theme, elevation),
+    [eventNameIn]: handleMouseDown,
+  };
+};
+
+export const ElevationPropsPress = (
+  stateHandler: {
+    state: { elevation: number };
+    setState: (v: any) => void;
+  },
+  inactive: number,
+  active: number,
+  customTheme: any
+) => {
+  return ElevationPropsToggle(
+    stateHandler,
+    inactive,
+    active,
+    'onMouseDown',
+    'mouseup',
+    customTheme
+  );
+};
+
+export const ElevationPropsHover = (
+  stateHandler: {
+    state: { elevation: number };
+    setState: (v: any) => void;
+  },
+  inactive: number,
+  active: number,
+  customTheme: any
+) => {
+  return ElevationPropsToggle(
+    stateHandler,
+    inactive,
+    active,
+    'onMouseOver',
+    'mouseout',
+    customTheme
+  );
+};
---
+++ stories/elevation.stories.tsx
@@ -0,0 +1,40 @@
+import { color, number } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import { css } from 'emotion';
+import * as React from 'react';
+import { ElevationProps } from '../src/elevation/elevation';
+import { mergeProps } from '../src/util/hoc.util';
+
+const classFlex = css`
+  display: flex;
+  justify-content: center;
+`;
+
+const squareClass = {
+  className: css`
+    height: 150px;
+    width: 150px;
+    background-color: grey;
+    border-radius: 4px;
+  `,
+};
+
+const knobNumberOpt = { min: 0, max: 24, step: 1, range: false };
+
+storiesOf('Props/Elevation', module)
+  .addDecorator(story => <div className={classFlex}>{story()}</div>)
+  .add('simple', () => (
+    <div {...mergeProps(squareClass, ElevationProps(number('Elevation', 5, knobNumberOpt)))} />
+  ))
+  .add('with custom theme', () => (
+    <div
+      {...mergeProps(
+        squareClass,
+        ElevationProps(number('Elevation', 11, knobNumberOpt), {
+          shadows: {
+            baselineColor: color('Shadow color', '#4527a0'),
+          },
+        })
+      )}
+    />
+  ));
