ab121b10bcd15326968499b3c61bcd40cea316a6
Draeken <PBM764927@gmail.com>
2019-04-09 18:13:34 +0200

    feat: add responsive utilities

---
+++ src/responsive/query-matcher.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+interface QueryMatcherProps {
+  mediaQuery: string;
+  defaultMatch?: boolean;
+  children?: (v: boolean) => React.ReactNode;
+  ToRender?: React.ComponentType<any>;
+  [key: string]: any;
+}
+
+export const QueryMatcherr: React.FunctionComponent<QueryMatcherProps> = props => {
+  const [matches, setMatches] = React.useState(!!props.defaultMatch);
+  const { children, ToRender, ...otherProps } = props;
+
+  let mql: MediaQueryList;
+  let handleMatchChangeFn: (a: any) => void;
+
+  React.useEffect(() => {
+    if (!window.matchMedia && matches !== props.defaultMatch) {
+      setMatches(!!props.defaultMatch);
+      return;
+    }
+    mql = window.matchMedia(props.mediaQuery);
+    setMatches(mql.matches);
+    handleMatchChangeFn = handleMatchChange(setMatches);
+    mql.addListener(handleMatchChangeFn);
+    return () => {
+      mql.removeListener(handleMatchChangeFn);
+    }
+  }, [props.mediaQuery, props.defaultMatch]);
+
+  if (children) {
+    return children(matches) as any;
+  }
+  if (ToRender && matches) {
+    delete otherProps.defaultMatch;
+    delete otherProps.mediaQuery;
+    return <ToRender {...otherProps} />;
+  }
+  return null;
+}
+
+const handleMatchChange = (setMatches: React.Dispatch<boolean>) => (e: MediaQueryList) => {
+  setMatches(e.matches);
+}
---
+++ src/responsive/responsive-theme.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { ThemeContext } from '../util/theme';
+
+interface ResponsiveThemeProps {
+  baseTheme: any;
+  rules: Array<{ key: string; query: string }>;
+  handleBreakpoint: (theme: any, key: string[]) => any;
+}
+
+export const ResponsiveTheme: React.FunctionComponent<ResponsiveThemeProps> = props => {
+  const { children } = props;
+  const [theme, setTheme] = React.useState(props.baseTheme);
+  let enabledRules: any;
+  let mqls: Array<{ key: string; mql: MediaQueryList }>;
+  let handleMatchChangeFn: (e: any) => void;
+
+  React.useEffect(() => {
+    if (!window.matchMedia) {
+      return;
+    }
+    mqls = props.rules.map(rule => ({
+      key: rule.key,
+      mql: window.matchMedia(rule.query),
+    }));
+    enabledRules = mqls.reduce(
+      (acc, cur) => ({ ...acc, [cur.key]: cur.mql.matches }),
+      {}
+    );
+    const initialTheme = props.handleBreakpoint(props.baseTheme, activeRules(enabledRules));
+    setTheme(initialTheme);
+    handleMatchChangeFn = handleMatchChange(props, enabledRules, setTheme, theme);
+    mqls.forEach(mql => mql.mql.addListener(handleMatchChangeFn));
+
+    return () => {
+      mqls.forEach(mql => mql.mql.removeListener(handleMatchChangeFn as any));
+    }
+  }, [props.baseTheme, props.handleBreakpoint, props.rules]);
+
+  return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
+}
+
+const activeRules = (enabledRules: any) => {
+  return Object.entries(enabledRules).reduce(
+    (acc, [rule, isMatched]) => (isMatched ? [...acc, rule] : acc),
+    [] as string[]
+  );
+}
+
+const handleMatchChange = (props: ResponsiveThemeProps, enabledRules: any, setTheme: React.Dispatch<any>, theme: any) => (e: MediaQueryList) => {
+  /**
+   *  Warning: if browser aren't unified about e.media formatting, it will not work.
+   *  The other way involves building a handleMatchChange function for each rule,
+   *  wich is cumbersome if you want to keep function reference for later listener removing.
+   *  Seriously, it would be way simpler to keep a subscription ID...
+   */
+  const rule = props.rules.find(r => r.query === e.media);
+  if (!rule) {
+    return;
+  }
+  enabledRules[rule.key] = e.matches;
+  setTheme({
+    theme: props.handleBreakpoint(theme, activeRules(enabledRules)),
+  });
+}
